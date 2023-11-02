cb7a4b807a8e6cc06e8aec4b3e65ad6cde749585
Hyune-c <choi8608@gmail.com>
2023-11-02 12:26:41 +0900

    init

---
+++ src/main/java/com/example/codingtesttemplate/CodingtestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.codingtesttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingtestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingtestTemplateApplication.class, args);
+    }
+
+}
